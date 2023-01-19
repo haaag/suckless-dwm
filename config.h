43d138c954c079ecd704039a709c65a7942f9117
haaag <haag_@tutanota.com>
2023-01-19 18:35:14 -0300

    dwm(patch): scratchpads n systray

--- config.h
+++ config.h
@@ -4,20 +4,39 @@
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const Gap default_gap        = {.isgap = 1, .realgap = 5, .gappx = 5};
 static const unsigned int snap      = 32;       /* snap pixel */
-static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
+static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayonleft = 0;   	/* 0: systray in the right corner, >0: systray on left of status text */
+static const unsigned int systrayspacing = 2;   /* systray spacing */
+static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+static const int showsystray        = 1;     /* 0 means no systray */
+static const int showbar            = 1;     /* 0 means no bar */
+static const int topbar             = 1;     /* 0 means bottom bar */
 static const char *fonts[]          = { "RobotoMono Nerd Font:size=10", "Material:size=12", "FontAwesome:size=11", "Font Awesome 5 Free:size=11", "Weather Icons:size=10"};
 static const char dmenufont[]       = "RobotoMono Nerd Font:size=10";
-static const char col_gray1[]       = "#24273A";
+static const char col_gray1[]       = "#222638";
 static const char col_gray2[]       = "#494D64";
 static const char col_gray3[]       = "#494D64";
 static const char col_gray4[]       = "#CAD3F5";
 static const char col_cyan[]        = "#8BD5CA";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray4, col_gray1, col_gray2 },
+	[SchemeNorm] = { col_gray4, col_gray1, col_gray1 },
 	[SchemeSel]  = { col_gray1, col_cyan,  col_cyan  },
 	[SchemeTitle]  = { col_gray1, col_gray1,  col_cyan  },
+};
+
+typedef struct {
+	const char *name;
+	const void *cmd;
+} Sp;
+const char *spcmd1[] = {"st", "-n", "spterm", "-g", "120x34", NULL };
+const char *spcmd2[] = {"st", "-n", "spfm", "-g", "144x41", "-e", "nnn", NULL };
+const char *spcmd3[] = {"st", "-n", "spmusic", "-g", "144x41", "-e", "ncmpcpp", NULL };
+static Sp scratchpads[] = {
+	/* name          cmd  */
+	{"spterm",      spcmd1},
+	{"spranger",    spcmd2},
+	{"keepassxc",   spcmd3},
 };
 
 /* tagging */
@@ -28,16 +47,24 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class					instance			title					tags mask     isfloating   monitor */
-	{ "Gimp",     				NULL,       		NULL,       			0,            1,           -1 },
-	{ "Firefox",  				NULL,       		NULL,       			1 << 8,       0,           -1 },
-	{ "TelegramDesktop",        NULL,               NULL,                   1 << 6,       0,           -1 },
-	{ "Signal",                 NULL,               NULL,                   1 << 6,       0,           -1 },
-	{ "tidal-hifi",             "tidal-hifi", 	    "tidal-hifi",           1 << 3,       0,           -1 },
-	{ "Galculator",             "galculator", 	    "galculator",           0,            1,           -1 },
-	{ "Gucharmap",              NULL,               NULL,                   0,            1,           -1 },
-	{ "mpv",                    NULL,               NULL,                   1 << 5,       1,           -1 },
-	{ "Sxiv",                   NULL,               NULL,                   0,            1,           -1 },
+	/* class				              instance	            title	            tags mask    isfloating   monitor */
+    { "Gimp",					          NULL,       	      NULL,       	      0,            1,            -1 },
+    { "Firefox",  		          NULL,       	      NULL,       	      1 << 8,       0,            -1 },
+    { "Nyxt",					          NULL,       	      NULL,       	      1 << 8,       0,            -1 },
+    { "TelegramDesktop",        NULL,               NULL,               1 << 6,       0,            -1 },
+    { "Signal",                 NULL,               NULL,               1 << 6,       0,            -1 },
+    { "tidal-hifi",             "tidal-hifi", 	    "tidal-hifi",       1 << 3,       0,            -1 },
+    { "Galculator",             "galculator", 	    "galculator",       0,            1,            -1 },
+    { "Gucharmap",              NULL,               NULL,               0,            1,            -1 },
+    { "mpv",                    NULL,               NULL,               0,            1,            -1 },
+    { "Peek",                   "peek",             NULL,               0,            1,            -1 },
+    { "Tk",                     "tk",               NULL,               0,            1,            -1 },
+    // { "mpv",                 NULL,               NULL,               1 << 5,       1,            -1 },
+    // { "mpv",                 "tuir",             "tuir",				      0,			      1,            -1 },
+    { "Sxiv",                   NULL,               NULL,               0,            1,            -1 },
+    { NULL,						          "spterm",			      NULL,				        SPTAG(0),	    1,            -1 },
+    { NULL,		  				        "spfm",		    	    NULL,		    	      SPTAG(1),	    1,            -1 },
+    { NULL,		  				        "spmusic",	    	  NULL,		    	      SPTAG(2),	    1,            -1 },
 };
 
 /* layout(s) */
@@ -67,19 +94,23 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 // static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *dmenucmd[] = { "dmenu_run", NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-p", "Command:", NULL };
+static const char *roficmd[] = { "rofi", "-show", "run", "-disable-history", NULL };
+// static const char *termcmd[]  = { "alacritty", NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *kittycmd[]  = { "kitty", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY|ShiftMask,             XK_p,      spawn,          {.v = roficmd } },
+	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = kittycmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY|ShiftMask,             XK_k,      rotatestack,    {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_j,      rotatestack,    {.i = +1 } },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	// { MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_d,      incnmaster,     {.i = +1 } },
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
@@ -101,6 +132,10 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_equal,  setgaps,        {.i = +5 } },
 	{ MODKEY|ShiftMask,             XK_minus,  setgaps,        {.i = GAP_RESET } },
 	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = GAP_TOGGLE} },
+	{ MODKEY,            		    XK_y,  	   togglescratch,  {.ui = 0 } },
+	{ MODKEY,            		    XK_u,      togglescratch,  {.ui = 1 } },
+	{ MODKEY,            		    XK_x,      togglescratch,  {.ui = 2 } },
+
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
@@ -117,13 +152,13 @@ static const Key keys[] = {
 /* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
 static const Button buttons[] = {
 	/* click                event mask      button          function        argument */
-	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
-	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
+	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
+	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
 	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
 	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
 	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
-	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
+	{ ClkClientWin,         MODKEY,         Button1,        resizemouse,    {0} },
 	{ ClkTagBar,            0,              Button1,        view,           {0} },
 	{ ClkTagBar,            0,              Button3,        toggleview,     {0} },
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
