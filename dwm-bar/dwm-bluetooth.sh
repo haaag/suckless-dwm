766e2bcecf15f398e45cc20a32741b9895aab724
haaag <haag_@tutanota.com>
2023-01-20 11:17:47 -0300

    dwm(scripts): dependency removed in bluetooth status
    Removed dependency with external script.

--- dwm-bar/dwm-bluetooth.sh
+++ dwm-bar/dwm-bluetooth.sh
@@ -1,42 +1,65 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 # Output: [ICON off]
 # Output: [ICON on]
 # Output: [ICON some-bt-device]
+# Output: [ICON some-bt-device,another-device]
 
-icon_on=""
+icon_on=""
+icon_device_connected=""
 icon_off=""
 
-bt_status=$(dwm-rofi-bt.sh --status)
-
-dwm_bluetooth_color() {
-	green_color="^c#8ec07c^"
-	bt_connected="^c#83a598^"
-	reset_color="^d^"
-
-	if [ "$bt_status" = "Off" ]; then
-		printf "%s%s Off%s" "$icon_off" "$SEP1" "$SEP2"
-	elif [ "$bt_status" = "" ]; then
-		printf "%s%s%s %sOn%s" "$SEP1" "$green_color" "$icon_on" "$reset_color" "$SEP2"
+power_on() {
+	if bluetoothctl show | grep -q "Powered: yes"; then
+		return 0
 	else
-		printf "%s%s%s %s%s%s" "$SEP1" "$bt_connected" "$icon_on" "$reset_color" "$bt_status" "$SEP2"
+		return 1
 	fi
 }
 
-dwm_bluetooth() {
-	printf "%s" "$SEP1"
-	if [ "$bt_status" = "Off" ]; then
-		printf "%s %s" "$icon_off" "$bt_status"
-	elif [ "$bt_status" = "" ]; then
-		printf "%s On" "$icon_on"
+device_connected() {
+	device_info=$(bluetoothctl info "$1")
+	if echo "$device_info" | grep -q "Connected: yes"; then
+		return 0
 	else
-		printf "%s %s" "$icon_on" "$bt_status"
+		return 1
 	fi
+}
+
+dwm_bt_status() {
+	printf "%s" "$SEP1"
+
+	if power_on; then
+		mapfile -t paired_devices < <(bluetoothctl devices | grep Device | cut -d ' ' -f 2)
+
+		counter=0
+
+		for device in "${paired_devices[@]}"; do
+			if device_connected "$device"; then
+				device_alias=$(bluetoothctl info "$device" | grep "Alias" | cut -d ' ' -f 2-)
+
+				if [ $counter -gt 0 ]; then
+					printf ",%s" "$device_alias"
+				else
+					printf "%s %s" "$icon_device_connected" "$device_alias"
+				fi
+				((counter++))
+			fi
+		done
+
+		if [ $counter -eq 0 ]; then
+			printf "%s On" "$icon_on"
+		fi
+
+	else
+		printf "%s Off" "$icon_off"
+	fi
+
 	printf "%s\n" "$SEP2"
 }
 
 if [ "$DWM_COLOR" = true ]; then
-	dwm_bluetooth_color
+	dwm_bt_status
 else
-	dwm_bluetooth
+	dwm_bt_status
 fi
