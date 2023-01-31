70dead048717c1d6bb656a72ea61f4a8130e2c1d
haaag <haag_@tutanota.com>
2023-01-30 21:06:46 -0300

    dwm(patch): 🪟 xresources
    This patch allows to handle settings from `Xresources`.
    It differs from xrdb patch in that it can handle any kind of setting as opposed to only color settings.

--- config.h
+++ config.h
@@ -2,7 +2,7 @@
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const Gap default_gap        = {.isgap = 1, .realgap = 5, .gappx = 5};
+static const Gap default_gap        = {.isgap = 1, .realgap = 6, .gappx = 6};
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayonleft = 0;   	/* 0: systray in the right corner, >0: systray on left of status text */
@@ -13,16 +13,27 @@ static const int showbar            = 1;     /* 0 means no bar */
 static const int topbar             = 1;     /* 0 means bottom bar */
 static const char *fonts[]          = { "RobotoMono Nerd Font:size=10", "Material:size=12", "FontAwesome:size=11", "Font Awesome 5 Free:size=11", "Weather Icons:size=10"};
 static const char dmenufont[]       = "RobotoMono Nerd Font:size=10";
-static const char col_gray1[]       = "#222638";
-static const char col_gray2[]       = "#494D64";
-static const char col_gray3[]       = "#494D64";
-static const char col_gray4[]       = "#CAD3F5";
-static const char col_cyan[]        = "#8BD5CA";
-static const char *colors[][3]      = {
+
+static char normbgcolor[]           = "#222222";
+static char normbordercolor[]       = "#444444";
+static char normfgcolor[]           = "#bbbbbb";
+static char selfgcolor[]            = "#eeeeee";
+static char selbordercolor[]        = "#005577";
+static char selbgcolor[]            = "#005577";
+static char *colors[][3] = {
+       /*               fg           bg           border   */
+       [SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
+       [SchemeSel]  = { selfgcolor,  selbgcolor,  selbordercolor  },
+/* static const char col_gray1[]       = "#222638"; */
+/* static const char col_gray2[]       = "#494D64"; */
+/* static const char col_gray3[]       = "#494D64"; */
+/* static const char col_gray4[]       = "#CAD3F5"; */
+/* static const char col_cyan[]        = "#8BD5CA"; */
+/* static const char *colors[][3]      = { */
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray4, col_gray1, col_gray1 },
-	[SchemeSel]  = { col_gray1, col_cyan,  col_cyan  },
-	[SchemeTitle]  = { col_gray1, col_gray1,  col_cyan  },
+	/* [SchemeNorm] = { col_gray4, col_gray1, col_gray1 }, */
+	/* [SchemeSel]  = { col_gray1, col_cyan,  col_cyan  }, */
+	/* [SchemeTitle]  = { col_gray1, col_gray1,  col_cyan  }, */
 };
 
 typedef struct {
@@ -50,6 +61,8 @@ static const Rule rules[] = {
 	/* class				              instance	            title	            tags mask    isfloating   monitor */
     { "Gimp",					          NULL,       	      NULL,       	      0,            1,            -1 },
     { "Firefox",  		          NULL,       	      NULL,       	      1 << 8,       0,            -1 },
+    { "librewolf",  		        NULL,       	      NULL,       	      1 << 8,       0,            -1 },
+    { "Tor Browser",  		      NULL,       	      NULL,       	      1 << 7,       0,            -1 },
     { "Nyxt",					          NULL,       	      NULL,       	      1 << 8,       0,            -1 },
     { "TelegramDesktop",        NULL,               NULL,               1 << 6,       0,            -1 },
     { "Signal",                 NULL,               NULL,               1 << 6,       0,            -1 },
@@ -93,12 +106,33 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-// static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+/* static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL }; */
 static const char *dmenucmd[] = { "dmenu_run", "-p", "Command:", NULL };
 static const char *roficmd[] = { "rofi", "-show", "run", "-disable-history", NULL };
-// static const char *termcmd[]  = { "alacritty", NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *kittycmd[]  = { "kitty", NULL };
+
+/*
+ * Xresources preferences to load at startup
+ */
+ResourcePref resources[] = {
+		/* { "font",               STRING,  &font }, */
+		{ "dmenufont",          STRING,  &dmenufont },
+		{ "normbgcolor",        STRING,  &normbgcolor },
+		{ "normbordercolor",    STRING,  &normbordercolor },
+		{ "normfgcolor",        STRING,  &normfgcolor },
+		{ "selbgcolor",         STRING,  &selbgcolor },
+		{ "selbordercolor",     STRING,  &selbordercolor },
+		{ "selfgcolor",         STRING,  &selfgcolor },
+		{ "borderpx",          	INTEGER, &borderpx },
+		{ "snap",          		INTEGER, &snap },
+		{ "showbar",          	INTEGER, &showbar },
+		{ "topbar",          	INTEGER, &topbar },
+		{ "nmaster",          	INTEGER, &nmaster },
+		{ "resizehints",       	INTEGER, &resizehints },
+		{ "mfact",      	 	FLOAT,   &mfact },
+};
+
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
