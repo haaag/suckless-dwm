577d46f869d33822e24df49251f7467f8a639a7c
haaag <haag_@tutanota.com>
2023-03-02 22:51:04 -0300

    feat(dwm): 🤲 `dwm-sticky` patch

--- config.h
+++ config.h
@@ -8,7 +8,7 @@ static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows sel
 static const unsigned int systrayonleft = 0;   	/* 0: systray in the right corner, >0: systray on left of status text */
 static const unsigned int systrayspacing = 2;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
-static const int showsystray        = 0;     /* 0 means no systray */
+static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;     /* 0 means no bar */
 static const int topbar             = 1;     /* 0 means bottom bar */
 static const char *fonts[]          = { "RobotoMono Nerd Font:size=10", "Material:size=12", "FontAwesome:size=11", "Font Awesome 5 Free:size=11", "Weather Icons:size=10"};
@@ -22,18 +22,9 @@ static char selbordercolor[]        = "#005577";
 static char selbgcolor[]            = "#005577";
 static char *colors[][3] = {
        /*               fg           bg           border   */
-       [SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
-       [SchemeSel]  = { selfgcolor,  selbgcolor,  selbordercolor  },
-/* static const char col_gray1[]       = "#222638"; */
-/* static const char col_gray2[]       = "#494D64"; */
-/* static const char col_gray3[]       = "#494D64"; */
-/* static const char col_gray4[]       = "#CAD3F5"; */
-/* static const char col_cyan[]        = "#8BD5CA"; */
-/* static const char *colors[][3]      = { */
-	/*               fg         bg         border   */
-	/* [SchemeNorm] = { col_gray4, col_gray1, col_gray1 }, */
-	/* [SchemeSel]  = { col_gray1, col_cyan,  col_cyan  }, */
-	/* [SchemeTitle]  = { col_gray1, col_gray1,  col_cyan  }, */
+      [SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
+      [SchemeSel]  = { selfgcolor,  selbgcolor,  selbordercolor  },
+      [SchemeTitle]  = { selbordercolor, normbgcolor, normbordercolor },
 };
 
 typedef struct {
@@ -43,12 +34,17 @@ typedef struct {
 const char *spcmd1[] = {"st", "-n", "spterm", "-g", "120x34", NULL };
 const char *spcmd2[] = {"st", "-n", "spfm", "-g", "144x41", "-e", "nnn", NULL };
 const char *spcmd3[] = {"st", "-n", "spmusic", "-g", "144x41", "-e", "ncmpcpp", NULL };
+const char *spcmd4[] = {"st", "-n", "spcode", "-g", "184x41", NULL };
 static Sp scratchpads[] = {
 	/* name          cmd  */
 	{"spterm",      spcmd1},
 	{"spranger",    spcmd2},
 	{"keepassxc",   spcmd3},
+	{"spcode",      spcmd4},
 };
+
+static const XPoint stickyicon[]    = { {0,0}, {4,0}, {4,8}, {2,6}, {0,8}, {0,0} }; /* represents the icon as an array of vertices */
+static const XPoint stickyiconbb    = {4,8};	/* defines the bottom right corner of the polygon's bounding box (speeds up scaling) */
 
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
@@ -58,12 +54,12 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class				              instance	            title	            tags mask    isfloating   monitor */
-    { "Gimp",					          NULL,       	      NULL,       	      0,            1,            -1 },
-    { "Firefox",  		          NULL,       	      NULL,       	      1 << 8,       0,            -1 },
-    { "librewolf",  		        NULL,       	      NULL,       	      1 << 8,       0,            -1 },
-    { "Tor Browser",  		      NULL,       	      NULL,       	      1 << 7,       0,            -1 },
-    { "Nyxt",					          NULL,       	      NULL,       	      1 << 8,       0,            -1 },
+	/* class				   instance	            title	            tags mask    isfloating   monitor */
+    { "Gimp",					NULL,       	    NULL,       	    0,            1,            -1 },
+    { "Firefox",  		        NULL,       	    NULL,       	    1 << 8,       0,            -1 },
+    { "librewolf",  		    NULL,       	    NULL,       	    1 << 8,       0,            -1 },
+    { "Tor Browser",  		    NULL,       	    NULL,       	    1 << 7,       0,            -1 },
+    { "Nyxt",					NULL,       	    NULL,       	    1 << 8,       0,            -1 },
     { "TelegramDesktop",        NULL,               NULL,               1 << 6,       0,            -1 },
     { "Signal",                 NULL,               NULL,               1 << 6,       0,            -1 },
     { "tidal-hifi",             "tidal-hifi", 	    "tidal-hifi",       1 << 3,       0,            -1 },
@@ -72,12 +68,11 @@ static const Rule rules[] = {
     { "mpv",                    NULL,               NULL,               0,            1,            -1 },
     { "Peek",                   "peek",             NULL,               0,            1,            -1 },
     { "Tk",                     "tk",               NULL,               0,            1,            -1 },
-    // { "mpv",                 NULL,               NULL,               1 << 5,       1,            -1 },
-    // { "mpv",                 "tuir",             "tuir",				      0,			      1,            -1 },
     { "Sxiv",                   NULL,               NULL,               0,            1,            -1 },
-    { NULL,						          "spterm",			      NULL,				        SPTAG(0),	    1,            -1 },
-    { NULL,		  				        "spfm",		    	    NULL,		    	      SPTAG(1),	    1,            -1 },
-    { NULL,		  				        "spmusic",	    	  NULL,		    	      SPTAG(2),	    1,            -1 },
+    { NULL,						"spterm",			NULL,				SPTAG(0),	  1,            -1 },
+    { NULL,		  				"spfm",		    	NULL,		    	SPTAG(1),	  1,            -1 },
+    { NULL,		  				"spmusic",	    	NULL,		    	SPTAG(2),	  1,            -1 },
+    { NULL,		  				"spcode",	    	NULL,		    	SPTAG(3),	  1,            -1 },
 };
 
 /* layout(s) */
@@ -88,7 +83,7 @@ static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen win
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[]=",      tile },    /* first entry is default */
+	{ "[]=",      tile },    /* Default: Master on left, slaves on right */
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
 };
@@ -106,31 +101,31 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-/* static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL }; */
 static const char *dmenucmd[] = { "dmenu_run", "-p", "Command:", NULL };
 static const char *roficmd[] = { "rofi", "-show", "run", "-disable-history", NULL };
 static const char *termcmd[]  = { "st", NULL };
-static const char *kittycmd[]  = { "kitty", NULL };
+// static const char *kittycmd[]  = { "kitty", NULL };
+// static const char *wezterm[]  = { "wezterm", NULL };
 
 /*
  * Xresources preferences to load at startup
  */
 ResourcePref resources[] = {
-		/* { "font",               STRING,  &font }, */
-		{ "dmenufont",          STRING,  &dmenufont },
-		{ "normbgcolor",        STRING,  &normbgcolor },
-		{ "normbordercolor",    STRING,  &normbordercolor },
-		{ "normfgcolor",        STRING,  &normfgcolor },
-		{ "selbgcolor",         STRING,  &selbgcolor },
-		{ "selbordercolor",     STRING,  &selbordercolor },
-		{ "selfgcolor",         STRING,  &selfgcolor },
-		{ "borderpx",          	INTEGER, &borderpx },
-		{ "snap",          		INTEGER, &snap },
-		{ "showbar",          	INTEGER, &showbar },
-		{ "topbar",          	INTEGER, &topbar },
-		{ "nmaster",          	INTEGER, &nmaster },
-		{ "resizehints",       	INTEGER, &resizehints },
-		{ "mfact",      	 	FLOAT,   &mfact },
+	/* { "font",               STRING,  &font }, */
+	{ "dmenufont",          STRING,  &dmenufont },
+	{ "normbgcolor",        STRING,  &normbgcolor },
+	{ "normbordercolor",    STRING,  &normbordercolor },
+	{ "normfgcolor",        STRING,  &normfgcolor },
+	{ "selbgcolor",         STRING,  &selbgcolor },
+	{ "selbordercolor",     STRING,  &selbordercolor },
+	{ "selfgcolor",         STRING,  &selfgcolor },
+	{ "borderpx",          	INTEGER, &borderpx },
+	{ "snap",          		INTEGER, &snap },
+	{ "showbar",          	INTEGER, &showbar },
+	{ "topbar",          	INTEGER, &topbar },
+	{ "nmaster",          	INTEGER, &nmaster },
+	{ "resizehints",       	INTEGER, &resizehints },
+	{ "mfact",      	 	FLOAT,   &mfact },
 };
 
 
@@ -138,22 +133,23 @@ static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_p,      spawn,          {.v = roficmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = kittycmd } },
+	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY|ShiftMask,             XK_k,      rotatestack,    {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_j,      rotatestack,    {.i = +1 } },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_d,      incnmaster,     {.i = +1 } },
+	{ MODKEY,						XK_g,		togglesticky,	{0} },
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} }, /* title */
+	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} }, /* floating */
+	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} }, /* monocle */
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
@@ -169,6 +165,7 @@ static const Key keys[] = {
 	{ MODKEY,            		    XK_y,  	   togglescratch,  {.ui = 0 } },
 	{ MODKEY,            		    XK_u,      togglescratch,  {.ui = 1 } },
 	{ MODKEY,            		    XK_x,      togglescratch,  {.ui = 2 } },
+	{ MODKEY|ShiftMask,            	XK_x,      togglescratch,  {.ui = 3 } },
 
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
@@ -198,4 +195,3 @@ static const Button buttons[] = {
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
-
