aeaadebadf4d01ce4ec3239812d33dab04f5c3c8
haaag <81921095+haaag@users.noreply.github.com>
2023-06-26 18:50:08 -0300

    🐌 minor updates

--- config.h
+++ config.h
@@ -15,7 +15,7 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 0;     /* 0 means no systray */
 static const int showbar            = 1;     /* 0 means no bar */
 static const int topbar             = 1;     /* 0 means bottom bar */
-static const char *fonts[]          = { "RobotoMono Nerd Font:size=10", "Material:size=12", "FontAwesome:size=11", "Font Awesome 5 Free:size=11", "Weather Icons:size=10"};
+static const char *fonts[]          = { "RobotoMono Nerd Font:size=10", "Material:size=12", "FontAwesome:size=14", "Font Awesome 5 Free:size=14", "Weather Icons:size=10"};
 static const char dmenufont[]       = "RobotoMono Nerd Font:size=10";
 static unsigned int baralpha        = 0xd0;
 static unsigned int borderalpha     = OPAQUE;
@@ -63,21 +63,19 @@ static const Rule rules[] = {
 	/* class				   instance	            title	            tags mask    isfloating   monitor */
     { "Gimp",					NULL,       	    NULL,       	    0,            1,            -1 },
     { "Firefox",  		        NULL,       	    NULL,       	    1 << 8,       0,            -1 },
-    { "Pale moon",  		    NULL,       	    NULL,       	    1 << 8,       0,            -1 },
     { "LibreWolf",  		    NULL,       	    NULL,       	    1 << 8,       0,            -1 },
     { "Tor Browser",  		    NULL,       	    NULL,       	    1 << 7,       0,            -1 },
-    { "Nyxt",					NULL,       	    NULL,       	    1 << 8,       0,            -1 },
     { "TelegramDesktop",        NULL,               NULL,               1 << 6,       0,            -1 },
     { "Signal",                 NULL,               NULL,               1 << 6,       0,            -1 },
     { "mpv",                    NULL,               NULL,               1 << 5,       0,            -1 },
     { "tidal-hifi",             "tidal-hifi", 	    "tidal-hifi",       1 << 3,       0,            -1 },
     { "Galculator",             "galculator", 	    "galculator",       0,            1,            -1 },
     { "Gucharmap",              NULL,               NULL,               0,            1,            -1 },
-    { "Pale moon",              NULL,               "Pale Moon Preferences",               0,            1,            -1 },
-    { "Pale moon",              NULL,               "Software Update",               0,            1,            -1 },
     { "Peek",                   "peek",             NULL,               0,            1,            -1 },
     { "Tk",                     "tk",               NULL,               0,            1,            -1 },
     { "Sxiv",                   NULL,               NULL,               0,            1,            -1 },
+    { "Nsxiv",                  NULL,               NULL,               0,            1,            -1 },
+    { "Timeshift-gtk",          "timeshift-gtk",    NULL,               0,            1,            -1 },
     { NULL,						"spterm",			NULL,				SPTAG(0),	  1,            -1 },
     { NULL,		  				"spfm",		    	NULL,		    	SPTAG(1),	  1,            -1 },
     { NULL,		  				"spmusic",	    	"ncmpcpp",			SPTAG(2),	  1,            -1 },
@@ -117,7 +115,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-p", "Command:", NULL };
 static const char *roficmd[] = { "rofi", "-show", "run", "-disable-history", NULL };
 static const char *termcmd[]  = { "st", NULL };
-static const char *dwmquit[] = { "dmenu-dwm-quit", NULL};
+static const char *dwmquit[] = { "dmenu-prompt", NULL};
 
 /*
  * Xresources preferences to load at startup
@@ -178,8 +176,8 @@ static const Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_minus,  setgaps,        {.i = GAP_RESET } },
 	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = GAP_TOGGLE} },
 	{ MODKEY,            		    XK_y,  	   togglescratch,  {.ui = 0 } },
-	// { MODKEY,            		    XK_u,      togglescratch,  {.ui = 1 } },
 	{ MODKEY,            		    XK_x,      togglescratch,  {.ui = 2 } },
+	// { MODKEY,            		    XK_u,      togglescratch,  {.ui = 1 } },
 	// { MODKEY,            		    XK_e,      togglescratch,  {.ui = 3 } },
 
 	TAGKEYS(                        XK_1,                      0)
